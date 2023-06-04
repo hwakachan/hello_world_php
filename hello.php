b5713c9f09bc7f944b3a0f4e5a8a4f6f04138a79
hwakachan <hi_wakachan0823@icloud.com>
2023-06-04 12:49:43 +0900

    use a variable

--- hello.php
+++ hello.php
@@ -1,4 +1,29 @@
+<html>
+<head>
+	<meta charset="utf-8"
+	<title>Sample3</title>
+</head>
+<body>
+	<h1>サンプルです。</h1>
+
 <?php
- echo "hello PHP!";
- echo "I'm Hikaru"
+  
+  $box1 = "waka";
+  $box2 = 100 - 50;
+  
+  echo "hello PHP!";
+  echo "<br>";
+  echo "I'm waka";
+
+  echo "<br>";
+
+  echo $box1;
+  echo "<br>";
+  echo $box2."<br>";
+
+  $box2++;
+  echo $box2;
 ?>
+
+</body>
+</html>
