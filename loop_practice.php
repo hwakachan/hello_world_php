d2505e46162767066b6911dbbf499ea7c8a55dd6
hwakachan <hi_wakachan0823@icloud.com>
2023-06-16 23:53:30 +0900

    practice nested loop

--- loop_practice.php
+++ loop_practice.php
@@ -43,5 +43,24 @@ do{
 } while($l < 3)
 ?>
 
+<br>
+<?php
+  $n = 5;
+for ($i = 1; $i <= $n; $i++) {
+  for($j = 1; $j <= $n; $j++){
+    if($i == 1 || $i == $n) {
+      echo "＊";
+    }else {
+      if($j == 1 || $j == $n) {
+        echo "＊";
+      }else {
+        echo "　";
+      }
+    }
+  }
+  echo "<br>";
+}
+?>
+
 </body>
 </html>
