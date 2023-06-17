004fdbf76dd5e93935094190ed81b2a6b0ac3515
hwakachan <hi_wakachan0823@icloud.com>
2023-06-17 14:58:07 +0900

    practice using function

---
+++ function_practice.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <h1>function</h1>
+</head>
+<body>
+<?php
+
+require "sample_function.php";
+
+$a = 4;
+$b = 2;
+$c = ($a - $b) / 2;
+
+echo return_word($b);
+?>
+
+</body>
+</html>
---
+++ sample_function.php
@@ -0,0 +1,12 @@
+<?php
+function return_word ($data) {
+
+  if ($data == 1){
+    return "a";
+  } else if ($data == 2) {
+    return "b";
+  } else {
+    return "c";
+  }
+}
+?>
