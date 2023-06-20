6767361c337bd7d771a278e3416801e2b9f40463
hwakachan <hi_wakachan0823@icloud.com>
2023-06-20 23:27:18 +0900

    add text in export file

--- fileExport.php
+++ fileExport.php
@@ -3,9 +3,11 @@
 <body>
 <?php
 
-$fp = fopen("test.txt", "w");
+//$fp = fopen("test.txt", "w");
+$fp = fopen("test.txt", "a");
 
-fwrite($fp, "this is test");
+//fwrite($fp, "this is test");
+fwrite($fp, "this \nis \ntest");
 
 fclose($fp);
 ?>
