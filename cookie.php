0d473d63659d87ee1ef84084bf75a67bcd38ff39
hwakachan <hi_wakachan0823@icloud.com>
2023-06-25 23:52:34 +0900

    learned cookie

---
+++ cookie.php
@@ -0,0 +1,12 @@
+<?php
+setcookie("count", 1);
+?>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>cookie</title>
+</head>
+<body>
+  <h1>cookie</h1>
+</body>
+</html>
---
+++ showCookie.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>show cookie</title>
+</head>
+<body>
+  <h1>show cookie</h1>
+
+  <?php
+    echo $_COOKIE["count"];
+  ?>
+</body>
+</html>
