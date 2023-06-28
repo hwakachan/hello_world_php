069abf8e2b32ad749ef6f9ba7334fc2eae4521dd
hwakachan <hi_wakachan0823@icloud.com>
2023-06-28 23:43:37 +0900

    start session

---
+++ session.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+$_SESSION["score"] = 80;
+
+?>
+<html>
+<body>
+<h1>session</h1>
+</body>
+</html>
---
+++ showSession.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>session</h1>
+<?php
+
+session_start();
+
+echo $_SESSION["score"];
+?>
+</body>
+</html>
